8a0d36920ab03ec3235a1cc5a5670d4cc867ea07
bettercallpaul22 <obaroiskool@gmail.com>
2023-12-30 14:36:42 +0100

    nodestorage

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
+import { LocalStorage } from 'node-localstorage'
 
-
+const localStorage = new LocalStorage('./scratch');
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
@@ -25,6 +26,8 @@ app.listen(PORT, ()=>console.log('listening on port 5000'))
 app.get('/', (req, res)=>{
     res.status(200).json("welcom skillGuardian homepage")
 })
-app.get('/post', (req, res)=>{
-    res.status(200).json("welcom skillGuardian homepage")
+app.post('/post', (req, res)=>{
+    localStorage.setItem("name", "obaro")
+  const value =  localStorage.getItem("name")
+    res.status(200).json( value)
 })
